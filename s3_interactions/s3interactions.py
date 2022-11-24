e8bf4ff36e7fdaf32505c6acad2ff2e806c7c068
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-24 08:50:12 +0000

    Adding tarfile member sanitization to extractall()

--- s3_interactions/s3interactions.py
+++ s3_interactions/s3interactions.py
@@ -102,7 +102,26 @@ def download_and_extract_model(model_uri: str, local_dir: str):
     os.makedirs(local_abs_model_dir, exist_ok=True)
     file_path = download_file(s3_path=key, bucket=bucket, local_dir=local_model_dir)
     with tarfile.open(file_path, mode="r") as tar:
-        tar.extractall(local_abs_model_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, local_abs_model_dir)
     return local_abs_model_dir
 
 
